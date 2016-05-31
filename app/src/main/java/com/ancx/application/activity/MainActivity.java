40b162d90894943f4f60ccd7c74929f19ab8b643
Ancx <acx1236@163.com>
2016-05-31 18:03:25 +0800

    圆形或圆角ImageView

--- app/src/main/java/com/ancx/application/activity/MainActivity.java
+++ app/src/main/java/com/ancx/application/activity/MainActivity.java
@@ -1,44 +1,49 @@
 package com.ancx.application.activity;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import com.ancx.ancxutil.utils.ImageLoader;
 import com.ancx.ancxutil.widget.LoadingWaitView;
+import com.ancx.ancxutil.widget.RoundImageView;
 import com.ancx.application.R;
 
 public class MainActivity extends AppCompatActivity implements LoadingWaitView.OnReloadDataListener {
 
-    private LoadingWaitView mLoadingWaitView;
+//    private LoadingWaitView mLoadingWaitView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RoundImageView mImageView = (RoundImageView) findViewById(R.id.mImageView);
+        ImageLoader.display("http://pic33.nipic.com/20130913/13008390_223449507127_2.jpg", mImageView);
+
 //        ImageView mImageView = (ImageView) findViewById(R.id.mImageView);
 //        ImageLoader.display("http://pic33.nipic.com/20130913/13008390_223449507127_2.jpg", mImageView);
 
-        mLoadingWaitView = (LoadingWaitView) findViewById(R.id.mLoadingWaitView);
-        mLoadingWaitView.setOnReloadDataListener(this);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                mLoadingWaitView.errorNet();
-            }
-        }, 5000);
+//        mLoadingWaitView = (LoadingWaitView) findViewById(R.id.mLoadingWaitView);
+//        mLoadingWaitView.setOnReloadDataListener(this);
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                mLoadingWaitView.errorNet();
+//            }
+//        }, 5000);
     }
 
-    private Handler handler = new Handler();
+//    private Handler handler = new Handler();
 
     @Override
     public void onReload() {
-        mLoadingWaitView.loading();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                mLoadingWaitView.loadComplete();
-            }
-        }, 5000);
+//        mLoadingWaitView.loading();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                mLoadingWaitView.loadComplete();
+//            }
+//        }, 5000);
     }
 }
